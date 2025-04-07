6376dffd085076befbff71f9afee6e1a09d93fa9
AhmetDevHub <ahmet.k.keskin@outlook.com>
2025-04-07 20:08:06 +0300

    char
    C#101 Lesson5

---
+++ Değişkenler_Char/Form1.cs
@@ -0,0 +1,21 @@
+namespace Değişkenler_Char
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //char takim;
+            //takim = 'g';
+            //label1.Text = takim.ToString();
+
+            //char ders;
+            //ders = Convert.ToChar(textBox1.Text);
+            //label1.Text = ders.ToString();
+        }
+    }
+}
